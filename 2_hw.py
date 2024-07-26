d743ab80cf56b116b9bf4005d81f3cc9f1540a89
TokyoWisp <annavacances86@gmail.com>
2024-07-26 19:23:22 +0300

    Update 2_hw.py

--- 2_hw.py
+++ 2_hw.py
@@ -1,1 +1,21 @@
+#1
+a: int = 2
+b: float = 3.25
+c: str = 'null'
+d: list = [2, 14]
+e: bool = (3 < 5)
 
+
+def task_1(a, b, c, d, e):
+    return (a, b, c, d, e)
+
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+print(type(e))
+
+print(task_1(a, b, c, d, e))
+
+
+#2
