594a8dab32ff6ba023163e8bc647c08f0cf6c658
TokyoWisp <annavacances86@gmail.com>
2024-07-26 20:01:59 +0300

    2_hw.py

--- 2_hw.py
+++ 2_hw.py
@@ -5,8 +5,7 @@ c: str = 'null'
 d: list = [2, 14]
 e: bool = (3 < 5)
 
-
-def task_1(a, b, c, d, e):
+def task_1(a, b, c, d, e) -> str:
     return (a, b, c, d, e)
 
 print(type(a))
@@ -15,7 +14,18 @@ print(type(c))
 print(type(d))
 print(type(e))
 
-print(task_1(a, b, c, d, e))
+print(task_1(3, 4.1, 'zero', [3,6], (8<3)))
 
 
 #2
+def task_2(a = [1, 2, 3, 5, 8, 13, 21]) -> int: #последовательность Фибоначчи
+    return (a[0:3])
+
+print(task_2())
+
+
+#3
+def task_3(a: int) -> int:
+    return (a ** 2)
+
+print(task_3(6))
